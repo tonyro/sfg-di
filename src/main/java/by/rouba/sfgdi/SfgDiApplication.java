c1f2e44d17a10c231fbf181b940d0bceffdcf088
tonyro <%%rsmbg9Xh9N29g9hUTUjFQ3mVypBf!KEkYdH#VK!ch#4*7>
2021-09-23 16:36:15 -0400

    Added simple Controller; used Spring Context to create the instance of it

--- src/main/java/by/rouba/sfgdi/SfgDiApplication.java
+++ src/main/java/by/rouba/sfgdi/SfgDiApplication.java
@@ -1,13 +1,19 @@
 package by.rouba.sfgdi;
 
+import by.rouba.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
---
+++ src/main/java/by/rouba/sfgdi/controllers/MyController.java
@@ -0,0 +1,12 @@
+package by.rouba.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println("Hello World!");
+
+        return "Hey Folks!";
+    }
+}
